c1943c8cdb56d2dc6b2418daaa5146f2f218c43b
Aric Chokey <achokey21@gmail.com>
2016-11-14 21:46:58 -0500

    limit particles based on screen size

--- js/particle.js
+++ js/particle.js
@@ -1,4 +1,7 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+// Limit particles to only work on tablet or bigger
+if (window.innerWidth > 768) {
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+}
